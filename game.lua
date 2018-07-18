742518ddef6747447cd801042e1df646fc67a032
IamTerror <32469633+IamTerror@users.noreply.github.com>
2018-07-18 13:14:48 +0200

    Add GetTileId() function + refactoring

--- game.lua
+++ game.lua
@@ -23,23 +23,22 @@ Game.Map = {
             {5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5},
           }
          
-  Game.TileTextures = {}
+Game.TileTextures = {}
   
-  function Game.Load()
-    print("Game:Chargement des textures...")
-    Game.TileTextures[0] = nil
-    Game.TileTextures[1] = love.graphics.newImage("images/green_mountain_1.png")
-    Game.TileTextures[2] = love.graphics.newImage("images/green_mountain_2.png")
-    Game.TileTextures[3] = love.graphics.newImage("images/green_mountain_3.png")
-    Game.TileTextures[4] = love.graphics.newImage("images/green_mountain_4.png")
-    Game.TileTextures[5] = love.graphics.newImage("images/green_mountain_5.png")
-    Game.TileTextures[6] = love.graphics.newImage("images/green_mountain_6.png")
-    Game.TileTextures[7] = love.graphics.newImage("images/sand.png")
-    Game.TileTextures[8] = love.graphics.newImage("images/door.png")
-    print("Game:Chargement des textures terminé")
+  function Game.GetTileId()
+    local x = love.mouse.getX()
+    local y = love.mouse.getY()
+    local col = math.floor(x / TILE_WIDTH) + 1
+    local lin = math.floor(y / TILE_HEIGHT) + 1
+    if col > 0 and col <= MAP_WIDTH and lin > 0 and lin <= MAP_HEIGHT then
+      local id = Game.Map[lin][col]
+      love.graphics.print("ID"..tostring(id),1,1)
+    else
+      love.graphics.print("Out of bounds",1,1)
+    end     
   end
   
-  function Game.Draw()
+  function Game.SetTiles()
     local l,c
     
     for l=1,MAP_HEIGHT do
@@ -51,6 +50,28 @@ Game.Map = {
         end
       end      
     end
+  end
+    
+  function Game.Load()
+    print("Game:Loading textures...")
+    Game.TileTextures[0] = nil
+    Game.TileTextures[1] = love.graphics.newImage("images/green_mountain_1.png")
+    Game.TileTextures[2] = love.graphics.newImage("images/green_mountain_2.png")
+    Game.TileTextures[3] = love.graphics.newImage("images/green_mountain_3.png")
+    Game.TileTextures[4] = love.graphics.newImage("images/green_mountain_4.png")
+    Game.TileTextures[5] = love.graphics.newImage("images/green_mountain_5.png")
+    Game.TileTextures[6] = love.graphics.newImage("images/green_mountain_6.png")
+    Game.TileTextures[7] = love.graphics.newImage("images/sand.png")
+    Game.TileTextures[8] = love.graphics.newImage("images/door.png")
+    print("Game:Finished loading textures")
+  end
+  
+  function Game.Draw()
+
+    Game.SetTiles()
+    
+    Game.GetTileId()
+    
   end
 
 return Game
